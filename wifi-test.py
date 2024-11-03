6895fdfd360be6de0dae8611663b1b63a161b5d9
hxxu0007 <hxxu0007@zonemail.clpccd.edu>
2024-11-02 20:09:45 -0700

    ver5

--- wifi-test.py
+++ wifi-test.py
@@ -59,7 +59,7 @@ def test_signal_speed():
     st = speedtest.Speedtest()
     st.get_best_server()
     download_speed = st.download() / 1_000_000 #convert to Mbps
-    uoload_speed = st.upload() / 1_000_000 #convert to Mbps
+    upload_speed = st.upload() / 1_000_000 #convert to Mbps
     ping = st.results.ping
     print(f"Download speed: {download_speed:.2f} Mbps")
     print(f"Upload speed: {upload_speed:.2f} Mbps")

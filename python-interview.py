75d0143dab6136bc9bf7cba1b94a306bc4e2f292
hxxu0007 <hxxu0007@zonemail.clpccd.edu>
2024-11-02 23:50:39 -0700

    v1

---
+++ python-interview.py
@@ -0,0 +1,13 @@
+n, m = 0, "abc"
+x, y, z = 0.125, "abc", False
+n = n + 1
+n += 1
+print("n =", n)
+print(f"x = {x:.2f}")  #0.12
+print(f"x = {x:.2%}")  #12.50%
+n = 0
+while n < 5:           #9,1,2,3,4
+    print(n)
+    n += 1
+for i in range(2,6):   #2,3,4,5
+    print(i)
